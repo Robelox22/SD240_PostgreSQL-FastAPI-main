321ee7a115baa88d3047f3d6e1a6b8a785def8b5
Robelox22 <136749121+Robelox22@users.noreply.github.com>
2024-11-26 13:43:35 -0600

    Ejercicio GET terminado

--- SD240_PostgreSQL-FastAPI-main/orm/repo.py
+++ SD240_PostgreSQL-FastAPI-main/orm/repo.py
@@ -12,6 +12,7 @@ def compra_por_id(sesion:Session,id_usuario:int):
     print("select * from app.compras where id = ", id_usuario)
     return sesion.query(modelos.Compra).filter(modelos.Compra.id==id_usuario).first()
 
+
 def foto_por_id(sesion:Session,id_usuario:int):
     print("select * from app.fotos where id = ", id_usuario)
     return sesion.query(modelos.Foto).filter(modelos.Foto.id==id_usuario).first()
